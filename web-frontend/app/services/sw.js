f71bb440dbefc489ab73c38395febc254fa4b268
Aaron Sikes <aaron@sikes.io>
2022-01-06 19:55:23 -0500

    temp

--- web-frontend/app/services/sw.js
+++ web-frontend/app/services/sw.js
@@ -6,43 +6,11 @@ import {action} from '@ember/object';
 
 if (!navigator.serviceWorker.controller) {
   console.log('no controlling worker!');
-  setTimeout(()=>location.reload(), 500);
+  // setTimeout(()=>location.reload(), 500);
 }
 
 export const ready = navigator.serviceWorker.ready.then(function(/*reg*/) {
   console.log('ready');
-  let send = (kind,value) => {
-    if (!navigator.serviceWorker.controller) {
-      // it'll reload in a sec
-      return;
-    }
-    if (value === undefined) {
-      navigator.serviceWorker.controller.postMessage(kind);
-    } else {
-      navigator.serviceWorker.controller.postMessage([kind,value])
-    }
-  };
-
-  // we want to avoid opening websockets and such for not-yet-active
-  // service workers. buuuut you can't actually tell from inside the
-  // service worker whether you're active or not. you can listen to
-  // the 'activate' event, but that only fires once, *ever*, per sw.
-  // the sw can be shut down due to no open tabs, then run again later,
-  // and have no way to tell that it's already been activated.
-  // so, we just ping from every active tab when they first start up,
-  // and that will trigger socket initialization in the sw if necessary.
-
-  send('init');
-
-  // these events aren't available within the service worker, but
-  // they're useful hints for websocket reconnection attempts,
-  // so we forward them along
-  window.addEventListener('online', () => send('online'));
-  window.addEventListener('offline', () => send('offline'));
-
-  // firefox shuts down service workers after 30 seconds of idle.
-  // but, we want it to keep the socket open in case of server events
-  setInterval(() => send('keepawake'), 25000);
 });
 
 export default class extends Service {
@@ -51,6 +19,8 @@ export default class extends Service {
 
   incoming_log = new ReplaySubject();
   outgoing_log = new ReplaySubject();
+
+  undelivered = [];
 
   @tracked
   change_log = [];
@@ -68,21 +38,44 @@ export default class extends Service {
         keys: Object.keys(e),
       });
       this.change_log = this.change_log;
+      for (let msg of this.undelivered) {
+        navigator.serviceWorker.controller.postMessage(msg);
+      }
+      this.undelivered = [];
     });
-
 
     fromEvent(navigator.serviceWorker, 'message', e => e.data)
       .subscribe(this.incoming);
 
-    this.outgoing.subscribe({
-      next(e) {
-        if (!navigator.serviceWorker.controller) {
-          console.error("No controlling service worker, to receive message:", e);
-          throw new Error('No controlling service worker!');
-        }
+    this.outgoing.subscribe((e) => {
+      if (navigator.serviceWorker.controller) {
         navigator.serviceWorker.controller.postMessage(e);
+      } else {
+        this.undelivered.push(e);
+        console.error("No controlling service worker, to receive message:", e);
+        // throw new Error('No controlling service worker!');
       }
     });
+
+    // these events aren't available within the service worker, but
+    // they're useful hints for websocket reconnection attempts,
+    // so we forward them along
+    window.addEventListener('online', () => this.send('online'));
+    window.addEventListener('offline', () => this.send('offline'));
+
+    // firefox shuts down service workers after 30 seconds of idle.
+    // but, we want it to keep the socket open in case of server events
+    setInterval(() => this.send('keepawake'), 25 * 1000);
+
+    // We want to avoid opening websockets and such for not-yet-active
+    // service workers. buuuut you can't actually tell from inside the
+    // service worker whether you're active or not. you can listen to
+    // the 'activate' event, but that only fires once, *ever*, per sw.
+    // the sw can be shut down due to no open tabs, then run again later,
+    // and have no way to tell that it's already been activated.
+    // so, we just ping from every active tab when they first start up,
+    // and that will trigger socket initialization in the sw if necessary.
+    this.send('init');
   }
 
   // event data comes through as a simple string (the event name),
